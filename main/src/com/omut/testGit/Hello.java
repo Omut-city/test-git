d11f81ba1941abc82c1021597c5f1286ea4fe55e
Alexander Pyankov <python239@mail.ru>
2017-02-05 03:38:37 +0300

    First commit

---
+++ main/src/com/omut/testGit/Hello.java
@@ -0,0 +1,12 @@
+package com.omut.testGit;
+
+/**
+ * @author Alexander Pyankov (python239@mail.ru)
+ * @version Alpha
+ * @date Initial record from 05.02.17
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
