eba646062cb272e85c8a175deb4e6c968904a573
Alexander Pyankov <python239@mail.ru>
2017-02-05 03:40:52 +0300

    Second commit

--- main/src/com/omut/testGit/Hello.java
+++ main/src/com/omut/testGit/Hello.java
@@ -8,5 +8,6 @@ package com.omut.testGit;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello everyone!!!");
     }
 }
