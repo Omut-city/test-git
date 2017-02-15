947f74ac19a7f6b00495fe7ed06b15941bf42f7b
Alexander Pyankov <python239@mail.ru>
2017-02-15 22:25:31 +0300

    addition

--- main/src/com/omut/testGit/Hello.java
+++ main/src/com/omut/testGit/Hello.java
@@ -9,6 +9,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello everyone!!!");
+        System.out.println("It's me!!!");
         System.out.println("************************************");
         System.out.println("************************************");
         System.out.println("************************************");
